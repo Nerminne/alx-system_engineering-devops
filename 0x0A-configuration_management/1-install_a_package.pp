401178670b1691a576364d3cf7d1ce885ac2a7e1
Nerminne <nerminee.mostafaa@gmail.com>
2024-03-22 19:10:55 -0700

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,10 @@
 # installing flask
-package { 'Werkzeug':
-  ensure => 2.1.1,
-}
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => package['Werkzeug'],
+
+package { 'Werkzeug':
+  ensure => 2.1.1,
 }
