9ab0fcdd0ec380750209ed24e306cf76e80fc6b4
Nerminne <138694722+Nerminne@users.noreply.github.com>
2024-03-25 04:26:20 +0200

    puppet config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# using puppet as in previous task
+
+file { '/etc/ssh/ssh_config':
+  ensure => 'present',
+  content => "
+    PasswordAuthentication no
+    IdentityFile ~/.ssh/school
+  ",
+}
