971ba38e6647715a724039188ec5bf843362202d
Nerminne <138694722+Nerminne@users.noreply.github.com>
2024-03-25 04:30:29 +0200

    puppet config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,11 @@
 # using puppet as in previous task
 
-file { '/etc/ssh/ssh_config':
-  ensure => 'present',
-  content => "
-    PasswordAuthentication no
-    IdentityFile ~/.ssh/school
-  ",
+file_line { 'Turn off password authentication':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
