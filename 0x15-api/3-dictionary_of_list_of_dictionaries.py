77dbc73d13bf9dbe1e6009bf311ccaf042854eaa
Nerminne <nerminee.mostafaa@gmail.com>
2024-05-20 22:03:16 -0700

    csv

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -22,23 +22,13 @@ def employee_username(empID):
 def all_todo():
     """adding all users inf to json"""
     response = requests.get("https://jsonplaceholder.typicode.com/todos")
-    ids = [1]
-    task = []
-    x = 0
-    for todos in response.json():
-        if todos["userId"] != ids[x]:
-            x += 1
-            ids.append(todos["userId"])
+    final = {}
     for todo in response.json():
-        task.append({"username": employee_username(todo["userId"]),
-                     "task": todo["title"],
-                     "completed": todo["completed"]})
-        final = {}
-    x = 0
-    for inf in task:
-        if x == (len(ids) - 1):
-            final[ids[x]] = inf
-            x += 1
+        if todo["userId"] not  in final:
+            final[todo["userId"]] = []
+        final[todo["userId"]].append({"username": employee_username(todo["userId"]),
+                                      "task": todo["title"],
+                                      "completed": todo["completed"]})
     filename = "todo_all_employees.json"
     with open(filename, "w") as f:
         json.dump(final, f)
